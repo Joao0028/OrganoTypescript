e58ad676cb89e874ce8e2ef43e65cd5b16abb1b5
Joao0028 <joaopaulonascimento720@gmail.com>
2023-10-12 19:06:49 -0300

    tipando o componente CampoTexto e Colaborador

--- src/componentes/CampoTexto/index.tsx
+++ src/componentes/CampoTexto/index.tsx
@@ -1,30 +1,31 @@
-import './CampoTexto.css'
+import "./CampoTexto.css";
 
 interface CampoTextoProps {
-    aoAlterado: (valor: string) => void
-    placeholder: string
-    label: string
-    valor: string
-    obrigatorio: boolean
+  aoAlterado: (valor: string) => void;
+  placeholder: string;
+  label: string;
+  valor: string;
+  obrigatorio: boolean;
 }
 
+const CampoTexto = ({ aoAlterado, label, obrigatorio, placeholder, valor }: CampoTextoProps) => {
+  const placeholderModificada = `${placeholder}...`;
 
-const CampoTexto = (props: CampoTextoProps) => {
+  const aoDigitado = (evento: React.ChangeEvent<HTMLInputElement>) => {
+    aoAlterado(evento.target.value);
+  };
 
-    const placeholderModificada = `${props.placeholder}...` 
+  return (
+    <div className="campo-texto">
+      <label>{label}</label>
+      <input
+        value={valor}
+        onChange={aoDigitado}
+        required={obrigatorio}
+        placeholder={placeholderModificada}
+      />
+    </div>
+  );
+};
 
-    const aoDigitado = (evento) => {
-        props.aoAlterado(evento.target.value)
-    }
-
-    return (
-        <div className="campo-texto">
-            <label>
-                {props.label}
-            </label>
-            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
-        </div>
-    )
-}
-
-export default CampoTexto
+export default CampoTexto;
